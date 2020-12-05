c9d9cb9d07441754e2224f4b993cc7c391045f3e
may arden <mayard3n@gmail.com>
2020-12-04 23:02:07 -0800

    commit tree c3a99574f675f1c14c97c24da21916d0ce5e0aac

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+    "My name is may"
+end
+
+get '/hometown' do
+    "My hometown is hood river"
+end
+
+get '/favorite-song' do
+    "My favorite song is careless whisper by george michael"
+end
+
+
 end
